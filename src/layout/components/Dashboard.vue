32c1c5c3da4a98bfc7386385d4d5e4c39761262e
zihao-xia <490082085@qq.com>
2022-10-01 21:23:38 +0800

    added dashboard contents

--- src/layout/components/Dashboard.vue
+++ src/layout/components/Dashboard.vue
@@ -1,35 +1,101 @@
 <template>
-    <div class="dashboard">
-        <div>
+    <div id="dashboard">
+        <div id="title">
             <h1>Team Dashboard</h1>
         </div>
-        <div>goals</div>
-        <div>score</div>
+        <div id="goals">
+            <ul id="goal-list">
+                <li v-for="goal in goals" :key="goal" class="goal">
+                    <div class="goal-title">
+                        <span class="goal-name">{{ goal.name }}</span>
+                        <span class="goal-due">Due date: {{ goal.due }}</span>
+                    </div>
+                    <div class="goal-description">Description:
+                        <div>{{ goal.description }}</div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div id="score">
+            <el-link id="score-title" :underline="false" href="#">Team Member & Team Health Score</el-link>
+            <HealthScoreChart />
+        </div>
     </div>
 </template>
 
 <script>
+import HealthScoreChart from './HealthScoreChart.vue'
+
 export default {
-  name: 'Dashboard'
+  name: 'Dashboard',
+  components: {
+      HealthScoreChart
+  },
+  data() {
+      return {
+          goals: [{name: 'Team Goal 1', due: 'xx/xx/xxxx', description: 'This is a description'},
+          {name: 'Team Goal 2', due: 'xx/xx/xxxx', description: 'This is a description'},
+          {name: 'Team Goal 3', due: 'xx/xx/xxxx', description: 'This is a description'},
+          {name: 'Team Goal 4', due: 'xx/xx/xxxx', description: 'This is a description'},
+          {name: 'Team Goal 5', due: 'xx/xx/xxxx', description: 'This is a description'},
+          {name: 'Team Goal 6', due: 'xx/xx/xxxx', description: 'This is a description'}]
+      }
+  }
+  
 }
 </script>
 
-<style lang="scss" scoped>
-.dashboard {
+<style scoped>
+#dashboard {
     width: 40%;
     border-left: 1px solid var(--el-border-color);
     display: flex;
     flex-direction: column;
-    justify-content: space-around;
+    justify-content: start;
     align-items: center;
-    div {
-        width: 80%;
-        text-align: center;
-        font-size: 2em;
-        padding: 40px 0;
-    }
-    div:nth-child(1), div:nth-child(2) {
-        border-bottom: 1px solid var(--el-border-color);
-    }
+}
+
+#title, #goals {
+    width: 80%;
+}
+
+#title, #score {
+    text-align: center;
+}
+
+#title {
+    font-size: 2em;
+    padding: 50px 0 50px;
+}
+
+#goals, #score {
+        padding-top: 20px;
+        height: 550px;
+}
+
+#title, #goals {
+    border-bottom: 1px solid var(--el-border-color);
+}
+
+#goal-list {
+    height: 520px;
+    overflow-y: scroll;
+}
+
+.goal {
+    list-style: none;
+    height: 100px;
+    padding: 5px;
+}
+
+.goal-title {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 20px;
+}
+
+#score-title {
+    font-size: 1.5em;
+    font-weight: bold;
 }
 </style>
---
+++ src/layout/components/HealthScoreChart.vue
@@ -0,0 +1,29 @@
+<template>
+    <Bar :chart-data="chartData" />
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'HealthScoreChart',
+  components: { Bar },
+  data() {
+    return {
+      chartData: {
+        labels: ['Team', 'Team Member 1', 'Team Member 2', 'Team Member 3', 'Team Member 4'],
+        datasets: [
+          {
+            label: 'Health Score',
+            backgroundColor: '#82D1F6',
+            data: [90, 100, 75, 80, 60]
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
